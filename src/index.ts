373917fcdad2b884c877f3da060692b214fab4c6
dlswn666 <injostar@naver.com>
2026-01-03 08:16:57 +0900

    옵션 변경

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ const app = express();
 
 // 미들웨어 설정
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(loggerMiddleware);
 
 // 라우트 설정
